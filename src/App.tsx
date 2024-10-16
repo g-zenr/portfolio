1f8d674976bbaeed75a08b59d32e57ee63725585
Rosada, Renz <renzrosada01@gmail.com>
2024-10-16 23:43:13 +0800

    Revert "Feat: added pages"
    This reverts commit c0bbe0ccb417118dac1a7a6b313ec581d4d624fa.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Skills from "./pages/Skills";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App: React.FC = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="bg-black text-white">
-      <Navbar />
-      <Home id="home" />
-      <About id="about" />
-      <Skills id="skills" />
-      <Projects id="projects" />
-      <Contact id="contact" />
-      <Footer />
-    </div>
-  );
-};
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
--- src/utils/generateToken.ts
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-import config from "../config/config";
-
-const generateToken = (userId: string) => {
-  return jwt.sign({ id: userId }, config.jwtSecret, { expiresIn: "1h" });
-};
-
-export default generateToken;
